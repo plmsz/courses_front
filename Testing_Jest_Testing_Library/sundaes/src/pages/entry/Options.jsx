85991f48613665a841a174c45073df8fda484838
Paloma <85586862+plmsz@users.noreply.github.com>
2022-04-22 14:17:11 -0300

    mock server

---
+++ Testing_Jest_Testing_Library/sundaes/src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+//in real life, use Enum for optionType, since has only two types
+const Options = ({ optionType }) => {
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:3030/${optionType}`)
+			.then((response) => setItems(response.data))
+			.catch((error) => {
+				//TODO handle error
+			});
+	}, [optionType]);
+	//TODO components
+	const ItemComponent = optionType === "scoops" ? <ScoopOption /> : null;
+
+	const optionItems = items.map((item) => (
+		<ItemComponent
+			key={item.name}
+			name={item.name}
+			imagePath={item.imagePath}
+		/>
+	));
+
+	return <Row>{optionItems}</Row>;
+};
+
+export default Options;
---
+++ Testing_Jest_Testing_Library/sundaes/src/pages/entry/ScoopOption.jsx
@@ -0,0 +1,13 @@
+import { Col } from "react-bootstrap";
+
+const ScoopOption = ({ name, imagePath }) => {
+	return (
+		<div>
+			<Col xs={12} sm={6} lg={3} style={{ textAlign: "center" }}>
+				<img src={`http://localhost:3030/${imagePath}`} alt={`${name} Scoop`} style={{width: '75%'}} />
+			</Col>
+		</div>
+	);
+};
+
+export default ScoopOption;
