ce22bfc822f1951bfe2a81c03f3067cb849ccafd
paloma.souza <paloma.souza@ambevtech.com.br>
2022-05-10 15:48:03 -0300

    button enabled after 1 scoop

--- Testing_Jest_Testing_Library/sundaes/src/pages/OrderEntry.jsx
+++ Testing_Jest_Testing_Library/sundaes/src/pages/OrderEntry.jsx
@@ -4,13 +4,19 @@ import { Button } from "react-bootstrap";
 
 const OrderEntry = ({ setOrderPhase }) => {
   const [orderDetails] = useOrderDetails();
+
+  const disabled = orderDetails.totals.scoops === "$0.00";
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType={"scoops"} />
       <Options optionType={"toppings"} />
       <h2>Grand Total: {orderDetails.totals.grandTotal}</h2>
-      <Button variant="dark" onClick={() => setOrderPhase(1)}>
+      <Button
+        variant="dark"
+        onClick={() => setOrderPhase(1)}
+        disabled={disabled}
+      >
         Order Sundae
       </Button>
     </div>
